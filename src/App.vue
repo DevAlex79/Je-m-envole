7b6ab110453c1a335d502d3cce38c453ccd8a783
Alexandra Masi <masialexandra@gmail.com>
2024-05-05 17:52:52 +0200

    Résolution duplication div id=app par suppression id=app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="app">
+  <div>
     <Header />
-    
+
     <router-view></router-view>
+
     <Article />
     <Footer />
-    
-    
+
   </div>
 </template>
 
@@ -36,41 +36,44 @@ export default {
 /* styles CSS globaux si nécessaire */
 #app {
   width: 70%;
-  margin: 0 auto; /* Ajoutez cette ligne pour centrer le contenu horizontalement */
+  margin: 0 auto;
+  /* Ajoutez cette ligne pour centrer le contenu horizontalement */
   border: 1px solid #000;
   border-radius: 5px 5px 5px 5px;
   background: #FFFFFF;
   font-family: Poppins;
-  padding: 20px; 
+  padding: 20px;
 }
 
 /*body{
     width: 70%; /* Ajustez la largeur comme vous le souhaitez */
-    /*margin: 0 auto; /* Ajoutez cette ligne pour centrer le contenu horizontalement */
-    /*border: 1px solid #000;
+/*margin: 0 auto; /* Ajoutez cette ligne pour centrer le contenu horizontalement */
+/*border: 1px solid #000;
     border-radius: 5px 5px 5px 5px;
     background: #FFFFFF;
     font-family: Poppins;
     padding: 20px; 
 }*/
 
-#logo{
-    display: flex;
-    justify-content: center;
-    flex-shrink: 0;
+#logo {
+  display: flex;
+  justify-content: center;
+  flex-shrink: 0;
 }
 
 header {
   display: flex;
-  justify-content: space-between; /* Ajuste la disposition pour mettre le logo et le menu à chaque extrémité */
-  align-items: center; /* Centre verticalement le contenu */
-  padding: 20px; /* Ajoute un espacement autour du contenu */
+  justify-content: space-between;
+  /* Ajuste la disposition pour mettre le logo et le menu à chaque extrémité */
+  align-items: center;
+  /* Centre verticalement le contenu */
+  padding: 20px;
+  /* Ajoute un espacement autour du contenu */
 }
 
 #menu {
   display: flex;
-  gap: 20px; /* Espacement entre les éléments du menu */
+  gap: 20px;
+  /* Espacement entre les éléments du menu */
 }
-
 </style>
-
